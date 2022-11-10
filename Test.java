76f7bc67dfac4e8541cbde0b2ad1d877aa91bb35
Sandesh Patil <patilsandesh55@gmail.com>
2022-11-10 16:13:06 +0530

    add my first commit through torotoise

--- Test.java
+++ Test.java
@@ -4,7 +4,8 @@ class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
-
+		System.out.println("Sandesh");
+		System.out.println("Patil");
 
 	}
 
